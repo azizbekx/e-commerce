4dfc250be62f96d83e43a235394eab236fb136af
Azizbek <azizbekxm@gmail.com>
2021-02-27 11:03:24 +0500

    initial commit

---
+++ starter_code/src/main/java/com/example/demo/controllers/OrderController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.UserOrder;
+import com.example.demo.model.persistence.repositories.OrderRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+	
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	
+	@PostMapping("/submit/{username}")
+	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+		User user = userRepository.findByUsername(username);
+		if(user == null) {
+			return ResponseEntity.notFound().build();
+		}
+		UserOrder order = UserOrder.createFromCart(user.getCart());
+		orderRepository.save(order);
+		return ResponseEntity.ok(order);
+	}
+	
+	@GetMapping("/history/{username}")
+	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
+		User user = userRepository.findByUsername(username);
+		if(user == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(orderRepository.findByUser(user));
+	}
+}
